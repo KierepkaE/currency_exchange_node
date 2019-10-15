d9d55e0163a0994ac1a71539fde8f0188f576e15
kierepkae <kierepkaedyta@gmail.com>
2019-10-15 12:38:36 +0100

    setup env for requesting currency value

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-// http://api.nbp.pl/api/exchangerates/tables/{table}/
+// `http://api.nbp.pl/api/exchangerates/rates/a/${code}/`
 const request = require('request');
+const validCodes = ['usd', 'eur', 'gbp', 'chf'];
+const code = process.argv[2];
+const isValid = validCodes.find(currency => currency === code) ? true : false;
+if (!isValid) process.exit();
+const url = `http://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`
+request(url, { json: true }, (err, res, body) => {
+  console.log(body)
+})
