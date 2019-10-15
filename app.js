8858dced7595a3cf97da66074c6e321516ccbb49
kierepkae <kierepkaedyta@gmail.com>
2019-10-15 12:44:59 +0100

    refactor

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const isValid = validCodes.find(currency => currency === code) ? true : false;
 if (!isValid) process.exit();
 const url = `http://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`
 request(url, { json: true }, (err, res, body) => {
-  const { currency } = body
-  const message = `Average value of ${currency} at ${body.rates[0].effectiveDate} is ${body.rates[0].mid} PLN `
+  const { currency, rates } = body
+  const message = `Average value of ${currency} at ${rates[0].effectiveDate} is ${rates[0].mid} PLN `
   console.log(message)
 })
